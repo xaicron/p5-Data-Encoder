a675261887a8f89c4c05bef98d685cf1cf327d79
xaicron <xaicron@gmail.com>
2010-12-02 18:28:13 +0900

    initial commit

---
+++ lib/Data/Encoder.pm
@@ -0,0 +1,42 @@
+package Data::Encoder;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Data::Encoder -
+
+=head1 SYNOPSIS
+
+  use Data::Encoder;
+
+=head1 DESCRIPTION
+
+Data::Encoder is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2010 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
